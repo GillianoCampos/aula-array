ae54efbc19069ed26a0b584fb2a50a995b46fa74
GillianoCampos <gillianoac@yahoo.com.br>
2021-07-04 16:42:10 -0300

    Aulas Arrays e Listas Mutaveis

--- api_collections/src/main/kotlin/one/digitalinnovation/collections/TesteFuncoesExtendidas.kt
+++ api_collections/src/main/kotlin/one/digitalinnovation/collections/TesteFuncoesExtendidas.kt
@@ -4,7 +4,7 @@ fun main() {
     val salarios = arrayOf(
         "2000".toBigDecimal(),
         "1500".toBigDecimal(),
-        "4000".toBigDecimal()
+        "8000".toBigDecimal()
     )
 
     println(salarios.somatoria())
